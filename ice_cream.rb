99fcba093e19cadd571f34fc10a2a34190aa4b75
Davida Marion <davidamarion@gmail.com>
2013-11-04 19:48:26 -0800

    added extra flavor functionality

--- ice_cream.rb
+++ ice_cream.rb
@@ -5,17 +5,18 @@ flavor = Hash.new
 name_hash = Hash.new
 
 	while i <= guest_num 
-		puts "Guest name:"
+		puts "Hey, what's your name?"
 		name = gets.chomp
-		puts "Favorite Flavor:"
-		fave = gets.chomp
+		puts "What's your favorite flavor? If you have more than one, please separate with a comma!"
+		fave = gets.chomp.split(/\,/)
 		flavor[name] = fave
-			
-			if name_hash.include?(fave)
-				name_hash[fave].push(name)
+		fave.each do |favr|	
+			if name_hash.include?(favr)
+				name_hash[favr].push(name)
 			else
-				name_hash[fave] = [name]
+				name_hash[favr] = [name]
 			end
+		end
 		
 		puts " -------"
 
@@ -25,7 +26,7 @@ name_hash = Hash.new
 	puts "What kind of report would you like? Alphabetical by name, or by flavor?"
 		report = gets.chomp
 		if report == "name"  #Sorts alphabetically by name
-			flavor.sort.each { |key,value| puts "#{key}'s favorite flavor is #{value}"} 
+			flavor.sort.each { |key,value| puts "#{key}'s enjoys #{value.join(" and ")}"} 
 		elsif report == "flavor"
 			name_hash.sort.each {|key,value| puts "#{key} is enjoyed by #{value.join(" and ")}!"}
 		end
